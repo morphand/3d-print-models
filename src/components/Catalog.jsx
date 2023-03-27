b0362a205771b44e8e73f8809418d8558f10efb9
morphand <75131830+morphand@users.noreply.github.com>
2023-03-27 16:14:15 +0300

    add basic Catalog component

---
+++ src/components/Catalog.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ModelCard from "./ModelCard";
+import styles from "../styles/Catalog.module.css";
+
+function Catalog() {
+  const [models, setModels] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/api/catalog")
+      .then((res) => res.json())
+      .then((res) => setModels(res))
+      .catch((e) => console.log(e));
+  }, []);
+  return (
+    <>
+      <h3>Catalog</h3>
+      <div className={styles["models"]}>
+        {models.length ? (
+          models.map((model) => <ModelCard key={model._id} model={model} />)
+        ) : (
+          <h3>
+            No models found. <Link to="/upload">Upload</Link> yours now.
+          </h3>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Catalog;
