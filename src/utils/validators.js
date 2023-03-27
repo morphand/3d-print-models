b6e93dd4fb3372f2f670d3041ef84b1c901e5481
morphand <75131830+morphand@users.noreply.github.com>
2023-03-27 16:14:44 +0300

    extend validators

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -8,6 +8,12 @@ export const REPEAT_PASSWORD_MIN_LENGTH = PASSWORD_MIN_LENGTH;
 export const REPEAT_PASSWORD_MAX_LENGTH = PASSWORD_MAX_LENGTH;
 export const EMAIL_MIN_LENGTH = 6;
 export const EMAIL_MAX_LENGTH = 256;
+export const ALLOWED_MODEL_EXTENSIONS = ["stl", "obj", "3mf"];
+export const ALLOWED_IMAGE_EXTENSIONS = ["jpg", "jpeg", "png", "webp"];
+export const MODEL_NAME_MIN_LENGTH = 2;
+export const MODEL_NAME_MAX_LENGTH = 45;
+export const MODEL_DESCRIPTION_MIN_LENGTH = 0;
+export const MODEL_DESCRIPTION_MAX_LENGTH = 2000;
 
 /**
  * @param {String} username
@@ -117,4 +123,50 @@ export function validateLoginDetails(username, password) {
   // Else return status true in the result.
   result.status = true;
   return result;
+}
+
+/**
+ * @param {String} modelName
+ * @returns {Boolean}
+ */
+export function validateModelName(modelName) {
+  modelName = modelName.trim();
+  if (
+    !modelName ||
+    modelName.length < MODEL_NAME_MIN_LENGTH ||
+    modelName.length > MODEL_NAME_MAX_LENGTH
+  ) {
+    return false;
+  }
+  return true;
+}
+
+/**
+ * @param {String} modelDescription
+ * @returns {Boolean}
+ */
+export function validateModelDescription(modelDescription) {
+  modelDescription = modelDescription.trim();
+  if (
+    !modelDescription ||
+    modelDescription.length < MODEL_DESCRIPTION_MIN_LENGTH ||
+    modelDescription.length > MODEL_DESCRIPTION_MAX_LENGTH
+  ) {
+    return false;
+  }
+  return true;
+}
+
+export function validateImage(fileName) {
+  fileName = fileName.trim();
+  const fileNameArray = fileName.split(".");
+  const fileExtension = fileNameArray.pop();
+  if (!fileName) {
+    return false;
+  }
+  if (!ALLOWED_IMAGE_EXTENSIONS.includes(fileExtension.toLowerCase())) {
+    return false;
+  }
+
+  return true;
 }
