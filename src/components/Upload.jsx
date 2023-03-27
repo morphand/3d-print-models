61af89632141c15f4321b2a79875af45b8b96b1d
morphand <75131830+morphand@users.noreply.github.com>
2023-03-27 16:23:20 +0300

    add features to Upload

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,14 +1,32 @@
 import { useRef, useState } from "react";
 import styles from "../styles/Form.module.css";
+import { validateImage } from "../utils/validators";
+import Toast from "./Toast";
 
 function Upload({ userId }) {
   const [filesSelected, setFilesSelected] = useState(null);
+  const [images, setImages] = useState(null);
+  const [errors, setErrors] = useState(false);
+  const [modelCreated, setModelCreated] = useState(false);
   const modelName = useRef();
   const modelDescription = useRef();
+  function handleImages(images) {
+    setErrors(false);
+    [...images].forEach((image, i) => {
+      if (!validateImage(image.name)) {
+        setErrors(true);
+      }
+    });
+    if (!errors) {
+      setImages(images);
+    }
+  }
   function handleSubmit(e) {
     if (userId) {
       e.preventDefault();
       const formData = new FormData();
+
+      // Set basic model info.
       formData.append(
         "modelInfo",
         JSON.stringify({
@@ -17,20 +35,45 @@ function Upload({ userId }) {
           modelDescription: modelDescription.current.value,
         })
       );
-      [...filesSelected].forEach((file, i) =>
-        formData.append(`file-${i}`, file, file.name)
-      );
-      fetch(`http://localhost:5000/api/user/${userId}/upload`, {
-        method: "POST",
-        body: formData,
-      })
-        .then((res) => res.json())
-        .then((data) => console.log(data));
+
+      // Add the model files to the form data.
+      [...filesSelected].forEach((file, i) => {
+        formData.append(`file-${i}`, file, file.name);
+      });
+
+      // Add the images to the form data.
+      [...images].forEach((image, i) => {
+        formData.append(`image-${i}`, image, image.name);
+      });
+
+      // Send request.
+      if (!errors) {
+        fetch(`http://localhost:5000/api/user/${userId}/upload`, {
+          method: "POST",
+          body: formData,
+        })
+          .then((res) => res.json())
+          .then((res) => {
+            setModelCreated(true);
+          });
+      }
     }
   }
   return (
     <div>
       <h3>Upload model</h3>
+      {errors && (
+        <Toast
+          header="Invalid files."
+          content="Some files contain invalid file extensions."
+        />
+      )}
+      {modelCreated && (
+        <Toast
+          header="Model created."
+          content="The model has been created successfully."
+        />
+      )}
       <form
         method="POST"
         encType="multipart/form-data"
@@ -44,7 +87,7 @@ function Upload({ userId }) {
           id="modelName"
           ref={modelName}
           minLength="2"
-          maxLength="100"
+          maxLength="45"
           placeholder="The name of your model."
           required
         />
@@ -57,7 +100,9 @@ function Upload({ userId }) {
           rows="10"
           placeholder="Describe your model here."
         ></textarea>
-        <label htmlFor="model">Files (allowed file formats: STL/OBJ/3MF)</label>
+        <label htmlFor="model">
+          Model files (allowed file formats: STL/OBJ/3MF)
+        </label>
         <input
           type="file"
           name="model"
@@ -65,6 +110,16 @@ function Upload({ userId }) {
           onChange={(e) => {
             setFilesSelected(e.target.files);
           }}
+          required
+        />
+        <label htmlFor="modelImages">Model images</label>
+        <input
+          type="file"
+          name="modelImages"
+          id="modelImages"
+          multiple
+          onChange={(e) => handleImages(e.target.files)}
+          required
         />
         <input type="submit" value="Upload" />
       </form>
