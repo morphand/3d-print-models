0efd32548f1ae404ca26e79aff834b0b848dac52
morphand <75131830+morphand@users.noreply.github.com>
2023-03-31 16:06:46 +0300

    add AuthContext

--- src/App.js
+++ src/App.js
@@ -11,48 +11,49 @@ import Upload from "./components/Upload/Upload";
 import Catalog from "./components/Catalog/Catalog";
 import Model from "./components/Model/Model";
 import EditProfile from "./components/Profile/EditProfile";
+import RouteGuard from "./components/RouteGuard";
+import AuthContext from "./contexts/Auth";
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem("auth"));
   const decodedToken = useJwt(token).decodedToken;
   const [_id, set_id] = useState("");
   const [username, setUsername] = useState("");
+
   useEffect(() => {
     if (decodedToken) {
       set_id(decodedToken._id);
       setUsername(decodedToken.username);
     }
   }, [decodedToken]);
-  const isUserLoggedIn = token ? true : false;
+
+  const contextValues = {
+    userId: _id,
+    username: username,
+    isUserLoggedIn: token ? true : false,
+  };
+
   return (
     <div className="App">
-      <Navigation
-        _id={_id}
-        username={username}
-        isUserLoggedIn={isUserLoggedIn}
-        setToken={setToken}
-      />
-      <main>
-        <div className="container">
-          <Routes>
-            <Route
-              path="/"
-              element={<Home isUserLoggedIn={isUserLoggedIn} />}
-            />
-            <Route path="/login" element={<Login setToken={setToken} />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/profile/:userId" element={<Profile />} />
-            <Route path="/profile/:userId/edit" element={<EditProfile />} />
-            {isUserLoggedIn && (
-              <Route path="/upload" element={<Upload userId={_id} />} />
-            )}
-            <Route path="/models" element={<Catalog />} />
-            <Route path="/models/:id" element={<Model />} />
-            <Route path="*" element={<h1>404</h1>} />
-          </Routes>
-        </div>
-      </main>
-      <Footer />
+      <AuthContext.Provider value={contextValues}>
+        <Navigation setToken={setToken} />
+        <main>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login setToken={setToken} />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/profile/:userId" element={<Profile />} />
+              <Route path="/profile/:userId/edit" element={<EditProfile />} />
+              <Route path="/upload" element={<Upload />} />
+              <Route path="/models" element={<Catalog />} />
+              <Route path="/models/:id" element={<Model />} />
+              <Route path="*" element={<h1>404</h1>} />
+            </Routes>
+          </div>
+        </main>
+        <Footer />
+      </AuthContext.Provider>
     </div>
   );
 }
