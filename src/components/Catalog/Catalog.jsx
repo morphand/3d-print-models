9e1d69f86741eb5efa78967abe08cc41797a35b6
morphand <75131830+morphand@users.noreply.github.com>
2023-04-02 00:18:18 +0300

    add authContext to Catalog

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import ModelCard from "../Model/ModelCard";
 import styles from "../../styles/Catalog.module.css";
+import AuthContext from "../../contexts/Auth";
 
 function Catalog() {
   const [models, setModels] = useState([]);
+  const authContext = useContext(AuthContext);
+  const isUserLoggedIn = authContext.isUserLoggedIn;
   useEffect(() => {
     fetch("http://localhost:5000/api/catalog")
       .then((res) => res.json())
@@ -17,9 +20,14 @@ function Catalog() {
       <div className={styles["models"]}>
         {models.length ? (
           models.map((model) => <ModelCard key={model._id} model={model} />)
-        ) : (
+        ) : isUserLoggedIn ? (
           <h3>
             No models found. <Link to="/upload">Upload</Link> yours now.
+          </h3>
+        ) : (
+          <h3>
+            No models found. <Link to="/login">Login</Link> or{" "}
+            <Link to="/register">register</Link> to upload yours now.
           </h3>
         )}
       </div>
