4beb999901c295492af13333c57f6a9b409ebcb6
morphand <75131830+morphand@users.noreply.github.com>
2023-04-02 13:52:27 +0300

    show delete comment button if the user is admin

--- src/components/Model/Comment.jsx
+++ src/components/Model/Comment.jsx
@@ -1,18 +1,28 @@
 import { DeleteIcon } from "../Icons/Icons";
 import styles from "../../styles/Model.module.css";
+import AuthContext from "../../contexts/Auth";
 import { dateFormatter } from "../../utils/formatters";
+import { useContext } from "react";
 
-function Comment({ comment }) {
+function Comment({ comment, onClick }) {
+  const authContext = useContext(AuthContext);
+  const isUserAdmin = authContext.isUserAdmin;
   return (
     <div
       key={comment._id}
       className={styles["model-comments-list-single-comment"]}
     >
-      <span className={styles["model-comments-list-single-comment-delete"]}>
-        <button>
-          <DeleteIcon />
-        </button>
-      </span>
+      {isUserAdmin && (
+        <span className={styles["model-comments-list-single-comment-delete"]}>
+          <button
+            onClick={(e) => {
+              onClick(e, comment);
+            }}
+          >
+            <DeleteIcon />
+          </button>
+        </span>
+      )}
       <span className="model-comments-list-single-comment-date">
         <small>{dateFormatter(comment.date)}</small>{" "}
       </span>
