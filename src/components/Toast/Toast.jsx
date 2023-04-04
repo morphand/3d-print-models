fb4b9a8d562af0cd94848a0fb8cae180855f2244
morphand <75131830+morphand@users.noreply.github.com>
2023-04-04 16:27:27 +0300

    make Toast component stateless

--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -1,24 +1,17 @@
-import { useState } from "react";
 import styles from "../../styles/Toast.module.css";
 
-function Toast({ header, content, hideAfterMs = 3000 }) {
-  const [isShown, setIsShown] = useState(true);
-  setTimeout(() => {
-    setIsShown(false);
-  }, hideAfterMs);
+function Toast({ header, content }) {
   return (
-    isShown && (
-      <div className={styles["toast"]}>
-        <div className={styles["toast-header"]}>
-          <p>{header}</p>
-        </div>
-        <div className="toast-content">
-          <p>
-            <small>{content}</small>
-          </p>
-        </div>
+    <div className={styles["toast"]}>
+      <div className={styles["toast-header"]}>
+        <p>{header}</p>
       </div>
-    )
+      <div className="toast-content">
+        <p>
+          <small>{content}</small>
+        </p>
+      </div>
+    </div>
   );
 }
 
