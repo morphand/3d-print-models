c81f39ad6bba444453704e9a4c07935a56596508
morphand <75131830+morphand@users.noreply.github.com>
2023-04-07 23:32:29 +0300

    add styles to the ModelCard compoennt

--- src/components/Model/ModelCard.jsx
+++ src/components/Model/ModelCard.jsx
@@ -1,20 +1,21 @@
 import { Link } from "react-router-dom";
 import { DownloadIcon, LikesIcon, CommentsIcon } from "../Icons/Icons";
+import styles from "../../styles/ModelCard.module.css";
 import { numberFormatter } from "../../utils/formatters";
 
 function ModelCard({ model }) {
   return (
-    <div className="model">
-      <div className="model-head">
+    <div className={styles["model"]}>
+      <div className={styles["model-head"]}>
         <span>{model.name}</span>
       </div>
-      <div className="model-uploaded-by">
+      <div className={styles["model-uploaded-by"]}>
         <small>Uploaded by {model.creator.username}</small>
       </div>
-      <div className="model-image">
+      <div className={styles["model-image"]}>
         <img src={model.images[0]} alt="" srcSet="" />
       </div>
-      <div className="model-statistics">
+      <div className={styles["model-statistics"]}>
         <p>
           <DownloadIcon /> <span>{numberFormatter(model.downloadsCount)}</span>
         </p>
