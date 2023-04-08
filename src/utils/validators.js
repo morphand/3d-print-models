e9df606772a7bfcf45796dc806cc455edc58a33e
morphand <75131830+morphand@users.noreply.github.com>
2023-04-08 12:28:44 +0300

    add validateModelFilename

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -8,7 +8,7 @@ export const REPEAT_PASSWORD_MIN_LENGTH = PASSWORD_MIN_LENGTH;
 export const REPEAT_PASSWORD_MAX_LENGTH = PASSWORD_MAX_LENGTH;
 export const EMAIL_MIN_LENGTH = 6;
 export const EMAIL_MAX_LENGTH = 256;
-export const ALLOWED_MODEL_EXTENSIONS = ["stl", "obj", "3mf"];
+export const ALLOWED_MODEL_EXTENSIONS = ["stl"];
 export const ALLOWED_IMAGE_EXTENSIONS = ["jpg", "jpeg", "png", "webp"];
 export const MODEL_NAME_MIN_LENGTH = 2;
 export const MODEL_NAME_MAX_LENGTH = 45;
@@ -280,6 +280,20 @@ export function validateImage(fileName) {
     return false;
   }
   if (!ALLOWED_IMAGE_EXTENSIONS.includes(fileExtension.toLowerCase())) {
+    return false;
+  }
+
+  return true;
+}
+
+export function validateModelFilename(fileName) {
+  fileName = fileName.trim();
+  const fileNameArray = fileName.split(".");
+  const fileExtension = fileNameArray.pop();
+  if (!fileName) {
+    return false;
+  }
+  if (!ALLOWED_MODEL_EXTENSIONS.includes(fileExtension.toLowerCase())) {
     return false;
   }
 
