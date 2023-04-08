b8fdcdce383d02c39fa8898b9dbdde18765e9b55
morphand <75131830+morphand@users.noreply.github.com>
2023-04-08 13:33:48 +0300

    change footer to provide the links to the client and server repositories

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,29 @@
+import { GitHubIcon } from "../Icons/Icons";
 import styles from "../../styles/Footer.module.css";
 
 function Footer() {
-  return <div className={styles["footer"]}>
-    <small>Footer</small>
-  </div>;
+  return (
+    <div className={styles["footer"]}>
+      <small>
+        This project is made for the SoftUni ReactJS exam. Checkout the code for
+        the <GitHubIcon />{" "}
+        <a
+          href="https://github.com/morphand/softuni-reactjs-exam"
+          target="_blank"
+        >
+          client
+        </a>{" "}
+        or the <GitHubIcon />{" "}
+        <a
+          href="https://github.com/morphand/softuni-reactjs-exam-server"
+          target="_blank"
+        >
+          server
+        </a>
+        .
+      </small>
+    </div>
+  );
 }
 
 export default Footer;
